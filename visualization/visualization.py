b657023d65f5552e81ee42d9a08e609c1400837e
Coquenim <nelson_coquenim@hotmail.com>
2018-06-29 12:09:58 +0100

    Upload do parsing do payload kafka
    Former-commit-id: bd8600589077d26e622a0aa6b57675c76cce8e1a

--- visualization/visualization.py
+++ visualization/visualization.py
@@ -7,11 +7,16 @@ Created on Fri Jun 29 10:27:31 2018
 """
 
 from kafka import KafkaConsumer
-
+import numpy as np
 
 def createConsumer(topic):
     return KafkaConsumer(topic)
 
+#ex: b'E0C54FD4238BC93F36CA25238F7E69C0-38,25'"
+def getPayload(msg):
+    payload = msg.decode('UTF-8') #converter bytes para str
+    return np.asarray(payload.split('||'))
+    
 
 #criacao dos consumidores para as varias queries
 
@@ -19,7 +24,8 @@ c_freq_routes = createConsumer('freq_routes')
 c_profit_areas = createConsumer('profit_areas')
 c_idle_taxis = createConsumer('idle_taxis')
 c_cong_areas = createConsumer('cong_areas')
-c_pleas_driver = createConsumer('pleas_driver')
+c_pleas_driver = createConsumer('pleasant_driver')
 
-#for msg in consumer:
-    #print (msg)
+for msg in c_pleas_driver:
+    payload = getPayload(msg.value)
+    print(payload[0])
